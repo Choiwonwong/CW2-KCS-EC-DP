29506706771af634eead37529e5b083a087dcf12
choi wonwong <chldnjsdnd1211@gmail.com>
2023-07-30 21:04:53 +0900

    날짜 관련 세부 수정

--- models/otas.py
+++ models/otas.py
@@ -1,25 +1,14 @@
 from pydantic import Field
-from fastapi import Form
-from datetime import datetime
+from datetime import datetime, timedelta
 from beanie import Document, PydanticObjectId
 
 class OTA(Document):
     id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
-    createdAt: datetime = datetime.now()
+    createdAt: datetime = Field(default_factory=lambda: datetime.now() + timedelta(hours=9))
     title: str
     authorName: str
     storeName: str
     memberCount: int = 1
 
-    @classmethod
-    def as_form(
-        cls,
-        title: str = Form(...),
-        authorName: str = Form(...),
-        storeName: str = Form(...),
-    ):
-        return cls(title=title, authorName=authorName,
-                   storeName=storeName)
     class Settings:
         name = "otas"
-
