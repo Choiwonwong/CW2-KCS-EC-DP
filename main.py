8314f40b1ffd865fb90516725a097ccba373638f
choi wonwong <chldnjsdnd1211@gmail.com>
2023-07-30 16:19:45 +0900

    Dockerfile 작성

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ app.include_router(otas.router, prefix='/otas')
 async def init_db():
     await settings.initialize_database()
 
-if __name__=="__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+# if __name__=="__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
